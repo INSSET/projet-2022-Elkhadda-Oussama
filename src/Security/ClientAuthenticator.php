8d4eb87b31265538262d9292843db433c79f65a3
oussama <74704768+Oussama3310@users.noreply.github.com>
2023-01-03 22:20:25 +0100

    reponse question1

--- src/Security/ClientAuthenticator.php
+++ src/Security/ClientAuthenticator.php
@@ -47,7 +47,7 @@ class ClientAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        return new RedirectResponse($this->urlGenerator->generate('product_list'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
